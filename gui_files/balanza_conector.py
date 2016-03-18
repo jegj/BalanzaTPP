a7d76579ed83f1acffcfe30b752cdd9f7cd35436
jegj <jegj57@gmail.com>
2016-03-18 16:12:13 -0500

    Cambio de tamaño de la interfaz grafica

--- gui_files/balanza_conector.py
+++ gui_files/balanza_conector.py
@@ -65,7 +65,7 @@ class SIO_Conector_de_Balanza:
         _ana1color = '#d9d9d9' # X11 color: 'gray85' 
         _ana2color = '#d9d9d9' # X11 color: 'gray85' 
 
-        top.geometry("720x500+376+183")
+        top.geometry("566x356+376+183")
         top.title("SIO: Conector de Balanza")
         top.configure(background="#dee1d9")
         top.configure(highlightbackground="#dbd9d9")
@@ -79,62 +79,61 @@ class SIO_Conector_de_Balanza:
         top.protocol("WM_DELETE_WINDOW", lambda: destroy_main_frame(top, self.log_mensajes, controller))
 
         self.Frame1 = Frame(top)
-        self.Frame1.place(relx=0.03, rely=0.02, relheight=0.23, relwidth=0.95)
+        self.Frame1.place(relx=0.04, rely=0.03, relheight=0.32, relwidth=0.93)
         self.Frame1.configure(relief=GROOVE)
         self.Frame1.configure(borderwidth="2")
         self.Frame1.configure(relief=GROOVE)
-        self.Frame1.configure(width=685)
+        self.Frame1.configure(width=525)
 
         self.Label2 = Label(self.Frame1)
-        self.Label2.place(relx=0.73, rely=0.13, height=19, width=57)
+        self.Label2.place(relx=0.63, rely=0.17, height=19, width=57)
         self.Label2.configure(activebackground="#f9f9f9")
         self.Label2.configure(text='''ESTADO:''')
 
         self.estado_conexion_msg = Message(self.Frame1)
-        self.estado_conexion_msg.place(relx=0.83, rely=0.03, relheight=0.37
-                , relwidth=0.15)
+        self.estado_conexion_msg.place(relx=0.78, rely=0.09, relheight=0.37
+                , relwidth=0.19)
         self.estado_conexion_msg.configure(foreground="#ee0809")
         self.estado_conexion_msg.configure(text='''DESCONECTADO''')
         self.estado_conexion_msg.configure(width=101)
 
         self.iniciar_button = Button(self.Frame1)
-        self.iniciar_button.place(relx=0.04, rely=0.17, height=27, width=177)
+        self.iniciar_button.place(relx=0.02, rely=0.17, height=27, width=137)
         self.iniciar_button.configure(activebackground="#d9d9d9")
         self.iniciar_button.configure(command=lambda: controller.start_socket_server(self.log_mensajes))
         self.iniciar_button.configure(text='''Iniciar/Reiniciar''')
 
         self.Message2 = Message(self.Frame1)
-        self.Message2.place(relx=0.55, rely=0.26, relheight=0.46, relwidth=0.27)
+        self.Message2.place(relx=0.4, rely=0.35, relheight=0.37, relwidth=0.36)
         self.Message2.configure(text='''ARCHIVO CONFIGURACION:''')
         self.Message2.configure(width=187)
 
         self.archivo_configuracion_msg = Label(self.Frame1)
-        self.archivo_configuracion_msg.place(relx=0.83, rely=0.42, height=19
-                , width=60)
+        self.archivo_configuracion_msg.place(relx=0.85, rely=0.44, height=19
+                , width=21)
         self.archivo_configuracion_msg.configure(activebackground="#f9f9f9")
         self.archivo_configuracion_msg.configure(text='''-''')
 
         self.Message4 = Message(self.Frame1)
-        self.Message4.place(relx=0.51, rely=0.7, relheight=0.2, relwidth=0.3)
+        self.Message4.place(relx=0.34, rely=0.7, relheight=0.2, relwidth=0.39)
         self.Message4.configure(text='''CARACTER ESTABILIDAD USADO:''')
         self.Message4.configure(width=205)
 
         self.caracter_estabilidad_msg = Label(self.Frame1)
-        self.caracter_estabilidad_msg.place(relx=0.81, rely=0.7, height=19
+        self.caracter_estabilidad_msg.place(relx=0.84, rely=0.7, height=19
                 , width=36)
         self.caracter_estabilidad_msg.configure(activebackground="#f9f9f9")
         self.caracter_estabilidad_msg.configure(text='''-''')
 
         self.Frame2 = Frame(top)
-        self.Frame2.place(relx=0.03, rely=0.28, relheight=0.65, relwidth=0.95)
+        self.Frame2.place(relx=0.04, rely=0.39, relheight=0.55, relwidth=0.93)
         self.Frame2.configure(relief=GROOVE)
         self.Frame2.configure(borderwidth="2")
         self.Frame2.configure(relief=GROOVE)
-        self.Frame2.configure(width=685)
+        self.Frame2.configure(width=525)
 
         self.log_mensajes = Text(self.Frame2)
-        self.log_mensajes.place(relx=0.01, rely=0.12, relheight=0.82
-                , relwidth=0.97)
+        self.log_mensajes.place(relx=0.02, rely=0.21, relheight=0.75, relwidth=0.96)
         self.log_mensajes.configure(background="white")
         self.log_mensajes.configure(font="TkTextFont")
         self.log_mensajes.configure(selectbackground="#c4c4c4")
@@ -143,7 +142,7 @@ class SIO_Conector_de_Balanza:
         self.log_mensajes.configure(state=DISABLED)
 
         self.Label1 = Label(self.Frame2)
-        self.Label1.place(relx=0.01, rely=0.03, height=19, width=71)
+        self.Label1.place(relx=0.02, rely=0.05, height=19, width=71)
         self.Label1.configure(activebackground="#f9f9f9")
         self.Label1.configure(text='''MENSAJES:''')
 
